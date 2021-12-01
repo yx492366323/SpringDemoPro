ed3c91387ac05781920921ee82b41122f29ea066
yx492 <yx492366323@qq.com>
2021-12-02 00:09:18 +0800

    写好登录逻辑

--- src/com/zjw/Handler/MyAuthenticationSuccessHandler.java
+++ src/com/zjw/Handler/MyAuthenticationSuccessHandler.java
@@ -18,14 +18,14 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         Set<String> Authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if(Authorities.contains("DEV")){
-            httpServletResponse.sendRedirect("./Hello/Git");
+            httpServletResponse.sendRedirect("../Hello/Git");
         }
         else{
             if(Authorities.contains("ADMIN")){
                 httpServletResponse.sendRedirect("/Admin/");
             }
             else{
-                    httpServletResponse.sendRedirect("./Hello/Hello");
+                    httpServletResponse.sendRedirect("../Hello/Hello");
             }
         }
     }
